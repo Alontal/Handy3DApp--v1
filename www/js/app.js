7efd6fa5a69d28d3fb1dd7a2dd94e4d9f630946a
RonenAlbagli <AlbagliRonen@gmail.com>
2017-05-01 15:00:05 +0300

    add: Take pic XXXX: upload taken pic to DB add: Photo Galllery

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+var app = angular.module('starter', ['ionic' ,'starter.controllers','ngCordova'])
 
   .run(function ($ionicPlatform) {
     $ionicPlatform.ready(function () {
@@ -84,7 +84,8 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         url: '/browse',
         views: {
           'menuContent': {
-            templateUrl: 'templates/browse.html'
+              templateUrl: 'templates/browse.html',
+              controller: 'GalleryCtrl'
           }
         }
       })
